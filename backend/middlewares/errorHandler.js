c32f0079db117f9f1678a432c532191455dbb39d
Sneha Chandra <snehachandra1108@gmail.com>
2025-10-25 10:24:03 +0530

    Create errorHandler.js

---
+++ backend/middlewares/errorHandler.js
@@ -0,0 +1,10 @@
+// Handles errors and sends proper response
+const errorHandler = (err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({
+    success: false,
+    message: err.message || "Internal Server Error"
+  });
+};
+
+module.exports = errorHandler;
